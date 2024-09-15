bd11023e3ed21bc95694351f470a982247f26dbb
juan-esteban-berger <juanestebanberger@gmail.com>
2024-09-15 08:38:21 -0600

    working on Sequential and Dense class

--- microkeras/layers.py
+++ microkeras/layers.py
@@ -4,7 +4,7 @@ class Dense:
     def __init__(self, units, activation=None, input_shape=None):
         self.units = units
         self.activation = activation
-        self.input_shape = input_shape
+        self.input_shape = input_shape[0]
         self.batch_size = None
         self.W = np.random.rand(units, input_shape) - 0.5
         self.b = np.random.rand(units, 1) - 0.5
