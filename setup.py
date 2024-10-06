8557d702fd7a2d5e80dd179278303616a8c93cbb
juan-esteban-berger <juanestebanberger@gmail.com>
2024-10-06 12:24:40 -0600

    0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="microkeras",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type='text/markdown',
